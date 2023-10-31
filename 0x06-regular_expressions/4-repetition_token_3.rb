632054eb7ed0b768aceca584d99cb8e395e8fa7f
walid oumechtak <woumechtak@gmail.com>
2023-10-31 02:10:26 -0700

    done

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/env ruby
-# match 0 or more t character
 puts ARGV[0].scan(/hbt*n/).join
--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/env ruby
-# match if there is h and any character btween with n "h.n"
 puts ARGV[0].scan(/h.n/).join
